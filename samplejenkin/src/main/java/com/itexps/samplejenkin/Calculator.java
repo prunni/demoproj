e436848efbca7aabb7dabee539f11a761b088786
prunni <108187962+prunni@users.noreply.github.com>
2022-07-25 10:32:44 -0500

    first version

---
+++ samplejenkin/src/main/java/com/itexps/samplejenkin/Calculator.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.itexps.samplejenkin;
+
+/**
+ *
+ * @author rajes
+ */
+public class Calculator {
+    public int add(int x,int y){
+        return x+y;
+    }
+}
